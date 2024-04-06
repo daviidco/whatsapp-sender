708951c35afdf59558e50d6b4c3586dd2711bd33
David Córdoba <daviidco@hotmail.com>
2024-04-06 13:01:03 -0500

    fix: check_selector_template

--- validatios_ui.py
+++ validatios_ui.py
@@ -2,7 +2,7 @@ import flet as ft
 
 
 def check_selector_template(selector_template: ft.Dropdown) -> bool:
-    if selector_template.value is None:
+    if selector_template.value in [None, ""]:
         selector_template.error_text = "Please select a template"
         selector_template.update()
         return False
