aebc098025aac6c8591a05d8ba3601f0ca6c6809
David Córdoba <daviidco@hotmail.com>
2024-04-06 23:13:13 -0500

    fix: refactor

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
 import flet as ft
 
-from controlsFlet import WhatsSenderApp
-from database import create_connection, create_table, insert_templates, check_if_templates_exist
-from scripts_sql import sql_create_templates_table, sql_query_count_templates, \
-    sql_create_sent_messages_table, sql_create_scheduled_messages_table
-from seed_templates import templates
+from database.conection import Database
+from database.repositories.template_reporitory import TemplateRepository
+from database.scripts_sql import sql_create_templates_table, sql_create_sent_messages_table, \
+    sql_create_scheduled_messages_table
+from pages.main_page.app_ui import UIComponentManager
 
 
 def main(page: ft.Page):
@@ -13,22 +13,25 @@ def main(page: ft.Page):
     """
     database = 'whatsapp-sender.db'
 
-    conn = create_connection(database)
+    db = Database(database)
+    template_repository = TemplateRepository(db)
 
     # create tables
-    if conn is not None:
+    if db.connection is not None:
         # create templates table
-        create_table(conn, sql_create_templates_table)
-        create_table(conn, sql_create_sent_messages_table)
-        create_table(conn, sql_create_scheduled_messages_table)
+        db.create_table(sql_create_templates_table)
+        db.create_table(sql_create_sent_messages_table)
+        db.create_table(sql_create_scheduled_messages_table)
 
-        if not check_if_templates_exist(conn, sql_query_count_templates):
-            insert_templates(conn, templates)
+        if not template_repository.check_if_templates_exist():
+            template_repository.insert_templates()
     else:
         print("Error! cannot create the database connection.")
 
     page.title = "Whatsapp Sender"
-    app = WhatsSenderApp(conn)
+
+    app = UIComponentManager(template_repository)
+
     page.add(app)
     page.update()
 
